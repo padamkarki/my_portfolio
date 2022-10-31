0fa35f7dc696662d465a0aa9227e5d63a2adcc32
Padam Karki <padamkarkipkkp@gmail.com>
2022-10-31 18:50:29 +0530

    made carousel for review section

--- components/Reviews.js
+++ components/Reviews.js
@@ -1,52 +1,9 @@
-import { Fragment } from "react";
+/* import { Fragment } from "react";
 import ReviewItem from "./ReviewItem";
-import sunit from "../assets/sunit.jpg";
-import vikram from "../assets/vikram.jpg";
-import ganesh from "../assets/ganesh.jpg";
-import priyanka from "../assets/priyanka.jpg";
 import styles from "./Work.module.css";
 import classes from "./ReviewItem.module.css";
 
 const Reviews = (props) => {
-  const clients = [
-    {
-      id: "r1",
-      img: sunit,
-      imgAlt: "Sunit",
-      clientName: "Sunit",
-      clientField: "Cinematographer",
-      clientReview:
-        "Quamquam id quidem, infinitum est in hac urbe, Ita multo sanguine profuso in laetitia et in victoria est mortuus. Atqui pugnantibus et contrariis studiis semper",
-      clientField: "Softare Engineer",
-    },
-    {
-      id: "r2",
-      img: vikram,
-      imgAlt: "Vikram",
-      clientName: "Vikram",
-      clientField: "Software Engineer",
-      clientReview:
-        "Quamquam id quidem, infinitum est in hac urbe, Ita multo sanguine profuso in laetitia et in victoria est mortuus. Atqui pugnantibus et contrariis studiis semper",
-    },
-    {
-      id: "r3",
-      img: ganesh,
-      imgAlt: "Ganesh",
-      clientName: "Ganesh",
-      clientField: "Software Engineer",
-      clientReview:
-        "Quamquam id quidem, infinitum est in hac urbe, Ita multo sanguine profuso in laetitia et in victoria est mortuus. Atqui pugnantibus et contrariis studiis semper",
-    },
-    {
-      id: "r4",
-      img: priyanka,
-      imgAlt: "Priyanka",
-      clientName: "Priyanka",
-      clientField: "Software Engineer",
-      clientReview:
-        "Quamquam id quidem, infinitum est in hac urbe, Ita multo sanguine profuso in laetitia et in victoria est mortuus. Atqui pugnantibus et contrariis studiis semper",
-    },
-  ];
   return (
     <>
       <div>
@@ -86,4 +43,117 @@ const Reviews = (props) => {
   );
 };
 
+
+
 export default Reviews;
+*/
+import React, { Component } from "react";
+import Slider from "react-slick";
+import { reviewsData } from "./reviewsData";
+import Image from "next/future/image";
+import Card from "./UI/Card";
+import "slick-carousel/slick/slick.css"; //react-slick css files
+import "slick-carousel/slick/slick-theme.css"; //react-slick css files
+import classes from "./ReviewItem.module.css";
+import left from "../assets/angle-left-solid.svg";
+import right from "../assets/angle-right-solid.svg";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+      }}
+      onClick={onClick}
+    >
+      {" "}
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        boxShadow: "1px 2px 9px #F4AAB9",
+        borderRadius: "50%",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+export default class Reviews extends Component {
+  render() {
+    const settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    };
+    return (
+      <>
+        <section>
+          <h2 className={classes.h2_hero_title}>Happy Clients</h2>
+          <Slider {...settings}>
+            {reviewsData.map((item) => (
+              <Card key={item.id} className={classes.review_card}>
+                <div>
+                  <div>
+                    <Image
+                      className={classes.review_img}
+                      src={item.img}
+                      alt={item.imgAlt}
+                      width={100}
+                      height={100}
+                    ></Image>
+                    <div>
+                      <h3 className={classes.client_name}>{item.clientName}</h3>
+                      <p className={classes.client_field}>{item.clientField}</p>
+                    </div>
+                  </div>
+                  <p className={classes.client_review}>{item.clientReview}</p>
+                </div>
+              </Card>
+            ))}
+          </Slider>
+        </section>
+      </>
+    );
+  }
+}
---
+++ components/reviewsData.js
@@ -0,0 +1,44 @@
+import sunit from "../assets/sunit.jpg";
+import vikram from "../assets/vikram.jpg";
+import ganesh from "../assets/ganesh.jpg";
+import priyanka from "../assets/priyanka.jpg";
+
+export const reviewsData = [
+  {
+    id: "r1",
+    img: sunit,
+    imgAlt: "Sunit",
+    clientName: "Sunit",
+    clientField: "Cinematographer",
+    clientReview:
+      "Quamquam id quidem, infinitum est in hac urbe, Ita multo sanguine profuso in laetitia et in victoria est mortuus. Atqui pugnantibus et contrariis studiis semper",
+    clientField: "Cinematographer",
+  },
+  {
+    id: "r2",
+    img: vikram,
+    imgAlt: "Vikram",
+    clientName: "Vikram",
+    clientField: "Software Engineer",
+    clientReview:
+      "Quamquam id quidem, infinitum est in hac urbe, Ita multo sanguine profuso in laetitia et in victoria est mortuus. Atqui pugnantibus et contrariis studiis semper",
+  },
+  {
+    id: "r3",
+    img: ganesh,
+    imgAlt: "Ganesh",
+    clientName: "Ganesh",
+    clientField: "Software Engineer",
+    clientReview:
+      "Quamquam id quidem, infinitum est in hac urbe, Ita multo sanguine profuso in laetitia et in victoria est mortuus. Atqui pugnantibus et contrariis studiis semper",
+  },
+  {
+    id: "r4",
+    img: priyanka,
+    imgAlt: "Priyanka",
+    clientName: "Priyanka",
+    clientField: "Software Engineer",
+    clientReview:
+      "Quamquam id quidem, infinitum est in hac urbe, Ita multo sanguine profuso in laetitia et in victoria est mortuus. Atqui pugnantibus et contrariis studiis semper",
+  },
+];
