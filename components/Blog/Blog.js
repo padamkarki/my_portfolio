5ebd0d38aaeb2138ab3f0d4d0ccc53295a967491
Padam Karki <padamkarkipkkp@gmail.com>
2022-12-27 14:03:01 +0530

    changed blog section in homepage with new component

--- components/Blog/Blog.js
+++ components/Blog/Blog.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
+
 import BlogTitle from "./BlogTitle";
-import BlogList from "./BlogList";
 import BlogDescription from "./BlogDescription";
+import BlogListHome from "./BlogListHome";
+
 import classes from "../Work/Work.module.css";
 import style from "../Blog/Blog.module.css";
 
@@ -15,7 +17,7 @@ const Blog = () => {
           <BlogDescription />
         </div>
         <div className={classes.limit}>
-          <BlogList />
+          <BlogListHome />
         </div>
         <div className={style.btn_center}>
           <Link href="/blog">
