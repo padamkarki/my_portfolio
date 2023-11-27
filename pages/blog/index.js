8350e910192ef5cf4b964478138bfdae3ba9b7f5
Padam Karki <padamkarkipkkp@gmail.com>
2023-11-27 16:45:55 +0530

    unnecessary code deleted

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,6 @@
 import BlogDescription from "../../components/Blog/BlogDescription";
 import BlogList from "../../components/Blog/BlogList";
 import classes from "./projects.module.css";
-import style from "../../components/Blog/Blog.module.css";
-import Form from "../../components/Form/Form";
 import HeroTitle from "../../components/UI/HeroTitle";
 import HeroDesc from "../../components/UI/HeroDesc";
 
@@ -16,7 +14,6 @@ const BlogPage = () => {
         </HeroDesc>
       </div>
       <BlogList />
-      {/* <Form /> */}
     </>
   );
 };
