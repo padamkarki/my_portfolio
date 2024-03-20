7b39636833045b96964ff7d220f5ad24c7857efb
Padam Karki <padamkarkipkkp@gmail.com>
2024-03-20 16:21:13 +0530

    added new website on projects

--- components/Work/workData.js
+++ components/Work/workData.js
@@ -1,10 +1,4 @@
-// import work_p1 from "../../assets/work_p1.jpeg";
-// import work_p2 from "../../assets/work_p2.jpeg";
-// import work_p3 from "../../assets/work_p3.jpeg";
-// import work_p4 from "../../assets/work_p4.jpeg";
-// import work_p5 from "../../assets/work_p5.jpeg";
-// import work_p6 from "../../assets/work_p6.jpeg";
-
+import fmdb from "../../assets/fmdb.jpg";
 import Little_lemon from "../../assets/Little_lemon.jpg";
 import Dashboard from "../../assets/Dashboard2.jpg";
 import Foody_app from "../../assets/Foody_app.jpg";
@@ -13,6 +7,19 @@ import Portfolio_2 from "../../assets/Portfolio_2.jpg";
 import Portfolio from "../../assets/Portfolio.jpg";
 
 export const projects = [
+  {
+    id: "fmdb",
+    img: fmdb,
+    imgAlt: "Website link",
+    imgInfo: "Free Movie Database App",
+    description:
+      "This Next.js-powered FMDB website offers users a seamless experience, featuring effortless CRUD operations for database management using APIs. Authentication is implemented using NextAuth.js for admin use cases. Tailwind CSS is utilized to ensure a sleek and modern interface. Real-time updates and smooth navigation are maintained, enhancing the database management experience",
+    url: "https://fmdbase.vercel.app/",
+    work: "Designer and Developer",
+    instruction:
+      "Enjoy free movies, explore a vast collection of movies available for free on legal online sources",
+    link: "https://fmdbase.vercel.app/",
+  },
   {
     id: "little_lemmon",
     img: Little_lemon,
@@ -90,87 +97,6 @@ export const projects = [
       "Navigate through different pages, Hover over and click on cards for checking animation",
     link: "https://padamkarki.github.io/my_portfolio/",
   },
-
-  // {
-  //   id: "p1",
-  //   img: work_p1,
-  //   imgAlt: "thumbnail",
-  //   imgInfo: "Teaser for Modern Car Servicing Studio | PSV",
-  //   description: "Teaser for Modern Car Servicing Studio | PSV",
-  //   url: "https://youtu.be/-2whcl7wWc8",
-  //   work: "Director & Editor",
-  //   embed: "https://www.youtube.com/embed/-2whcl7wWc8",
-  //   instruction: "Login with random email Id and password",
-  //   link: "https://dashboard-project-jet.vercel.app/dashboard",
-  // },
-  // {
-  //   id: "p2",
-  //   img: work_p2,
-  //   imgAlt: "thumbnail",
-  //   imgInfo: "Promotional / Advertisement video for Spring Mountain Farm | PSV",
-  //   description:
-  //     "Promotional / Advertisement video for Spring Mountain Farm | PSV",
-  //   url: "https://youtu.be/y7cwBL2vNtI",
-  //   work: "Director & Editor",
-  //   embed: "https://www.youtube.com/embed/y7cwBL2vNtI",
-  //   instruction: "Login with random email Id and password",
-  //   link: "https://dashboard-project-jet.vercel.app/dashboard",
-  // },
-  // {
-  //   id: "p3",
-  //   img: work_p3,
-  //   imgAlt: "thumbnail",
-  //   imgInfo:
-  //     "Promotional video  / Advertisement for Modern Car - Multi Brand Car Servicing Studio | PSV",
-  //   description:
-  //     "Promotional video  / Advertisement for Modern Car - Multi Brand Car Servicing Studio | PSV",
-  //   url: "https://youtu.be/5AaHbHCvbNg",
-  //   work: "Director & Editor",
-  //   embed: "https://www.youtube.com/embed/5AaHbHCvbNg",
-  //   instruction: "Login with random email Id and password",
-  //   link: "https://dashboard-project-jet.vercel.app/dashboard",
-  // },
-  // {
-  //   id: "p4",
-  //   img: work_p4,
-  //   imgAlt: "thumbnail",
-  //   imgInfo:
-  //     "Ganesh and Priyanka | Most Elegant and Beautiful wedding | Maharashtra Wedding | Wedding Highlight",
-  //   description:
-  //     "Ganesh and Priyanka wedding highlight. Best cinematic wedding video | Maharashtrian Indian Wedding 2022. Most Elegant and Beautiful wedding. Emotion is the true substance of wedding photography” and we at PSV Candid Studio strongly believe in the same. Wedding photography to us is to witness your beautiful story and capture it with its true essence. We listen to what you want and then create beautiful memories of your dream wedding. Your search for the best wedding photographer ends here.",
-  //   url: "https://youtu.be/MBAvwS_7z-E",
-  //   work: "Director & Editor",
-  //   embed: "https://www.youtube.com/embed/MBAvwS_7z-E",
-  //   instruction: "Login with random email Id and password",
-  //   link: "https://dashboard-project-jet.vercel.app/dashboard",
-  // },
-  // {
-  //   id: "p5",
-  //   img: work_p5,
-  //   imgAlt: "thumbnail",
-  //   imgInfo: "Logo Design and Intro Video for Amy Photography",
-  //   description: "Logo Design and Intro Video for Amy Photography.",
-  //   url: "https://youtu.be/XI6ySOxE9VE",
-  //   work: "Director & Editor",
-  //   embed: "https://www.youtube.com/embed/XI6ySOxE9VE",
-  //   instruction: "Login with random email Id and password",
-  //   link: "https://dashboard-project-jet.vercel.app/dashboard",
-  // },
-  // {
-  //   id: "p6",
-  //   img: work_p6,
-  //   imgAlt: "thumbnail",
-  //   imgInfo: "Pavan and Vrushali | Best Pre-Wedding of 2022",
-  //   description:
-  //     "Pavan and Vrushali | Best Pre-Wedding of 2022. Thank you for letting us share on your special day. We wish you all the happiness in the world. Here's to a lifetime full of happiness and love to the best couple  - 'Pavan and Vrushali'. Enjoy the best Pre-wedding of the era 😍😍😍",
-  //   url: "https://youtu.be/F3ubfPktmYQ",
-  //   work: "Director & Editor",
-  //   embed: "https://www.youtube.com/embed/F3ubfPktmYQ",
-  //   instruction: "Login with random email Id and password",
-  //   link: "https://dashboard-project-jet.vercel.app/dashboard",
-  // },
-
-  // // ------------Demo-------------------------------------------------------
 ];
 
 export const webProjects = [];
