f3a0487fe4969b85c526e74a19206c9c89f76021
Padam Karki <padamkarkipkkp@gmail.com>
2024-03-20 18:50:20 +0530

    favicon bug issue

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,12 +1,13 @@
 import classes from "./Layout.module.css";
 import Footer from "./Footer";
 import Head from "next/head";
+import favicon from "../../public/favicon_io/favicon.ico";
 
 const Layout = (props) => {
   return (
     <div>
       <Head>
-        <link rel="icon" href="/favicon_io/favicon.ico" />
+        <link rel="icon" href={favicon} />
       </Head>
       <main className={classes.main}>{props.children}</main>
       <Footer />
